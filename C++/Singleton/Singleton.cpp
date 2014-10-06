4e018ade3e60f0d2b9b9a8e83248eaf791f6477f
Kei <Kei@kuru-kuru.(none)>
2014-10-06 11:43:44 +0900

    edit Singleton.cpp

--- C++/Singleton/Singleton.cpp
+++ C++/Singleton/Singleton.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <cstring>
 #include <cstdlib>
 using std::cout;
 using std::endl;
+using std::string;
+
+class Singleton
+{
+  string msg_;
+
+  Singleton(); //Prevent clients from creating a new singleton
+  Singleton(const Singleton& rhs); //Prevent Clients from creating
+  Singleton& operator=(const Singleton& rhs); //Prevent Clients from copy
+
+public:
+  static Singleton& getInstance(void)
+  {
+    static Singleton obj;
+    return &obj;
+  }
+
+  void setString(string msg)
+  {
+    this -> msg_ = msg;
+  }
+
+  void printString()
+  {
+    cout << this->msg_ << endl;
+  }
+};
 
 int main()
 {
-  cout << "Hello World!!" << endl;
+  Singleton i = Singleton::getInstance();
+  i.setString("Hello");
+  i.printString();
   return EXIT_SUCCESS;
 }
