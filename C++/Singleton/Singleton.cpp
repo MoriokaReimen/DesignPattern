3382b9932920bfd272fb573d95e8b3b80e04693c
kei <menschenajger.mark.neun@gmail.com>
2014-10-06 13:01:56 +0900

    Add Singleton.cpp

--- C++/Singleton/Singleton.cpp
+++ C++/Singleton/Singleton.cpp
@@ -1,23 +1,37 @@
+/*
+ * Meyers Singleton with dead reference detect
+ * Author: reimen
+ * Data: Oct.6.2014
+ *
+ */
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <stdexcept>
 using std::cout;
 using std::endl;
 using std::string;
+using std::runtime_error;
 
 class Singleton
 {
+
+  static bool destroyed_; // flag for destruction detection
+
+  Singleton() {} //Prevent clients from creating a new singleton
+  Singleton(const Singleton& other) {} //Prevent Clients from creating
+  Singleton &operator=(const Singleton &rhs); //Prevent Clients from copy
+  ~Singleton() { destroyed_ = true; } // Prevent destruction
+
   string msg_;
-
-  Singleton(); //Prevent clients from creating a new singleton
-  Singleton(const Singleton& rhs); //Prevent Clients from creating
-  Singleton& operator=(const Singleton& rhs); //Prevent Clients from copy
-
 public:
-  static Singleton& getInstance(void)
+  static Singleton &getInstance()
   {
-    static Singleton obj;
-    return &obj;
+    static Singleton instance;
+    if(destroyed_)
+      throw std::runtime_error("Singleton: Dead Reference Access");
+    else
+      return instance;
   }
 
   void setString(string msg)
@@ -31,10 +45,15 @@ public:
   }
 };
 
+bool Singleton::destroyed_ = false; //Initialize flag
+
 int main()
 {
-  Singleton i = Singleton::getInstance();
+  Singleton &i = Singleton::getInstance();
+  Singleton &j = Singleton::getInstance();
   i.setString("Hello");
+  i.printString();
+  j.setString("Bye");
   i.printString();
   return EXIT_SUCCESS;
 }
