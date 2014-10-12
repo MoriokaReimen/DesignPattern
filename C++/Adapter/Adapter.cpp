3ba82c158125ce67b76b1ae2ed1a97502a3601e4
Kei Nakata <menschenjager.mark.neun@gmail.com>
2014-10-12 21:34:51 +0900

    Add Adapter.cpp

---
+++ C++/Adapter/Adapter.cpp
@@ -0,0 +1,54 @@
+/*
+ * Adapter Pattern
+ * Author: Kei Nakata
+ * Date: Oct.6.2014
+ * Convert the interface of a class into another interface clients expect.
+ * Adapter lets classes work together that couldn't otherwise because of incompatible
+ * interfaces.
+ */
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <stack>
+using std::cout;
+using std::endl;
+
+
+class Target
+{
+public:
+    virtual ~Target() {}
+    virtual void show()
+    {
+        cout << "World" << endl;
+    }
+};
+
+class Adaptee
+{
+public:
+    virtual ~Adaptee() {}
+    void print()
+    {
+        cout << "Heelo";
+    }
+};
+
+class Adapter : public Target, private Adaptee
+{
+public:
+    virtual ~Adapter() {}
+    virtual void show()
+    {
+        Adaptee::print();
+        cout << " ";
+        Target::show();
+    }
+};
+int main()
+{
+    Adapter a;
+    a.show();
+
+  return EXIT_SUCCESS;
+}
