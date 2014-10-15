796fd13ce2e88a333459513d41340c68e047132c
kei <menschenajger.mark.neun@gmail.com>
2014-10-15 12:23:21 +0900

    Add Observer.cpp

---
+++ C++/Observer/Observer.cpp
@@ -0,0 +1,76 @@
+/*
+ * Observer
+ * Author: reimen
+ * Data: Oct.15.2014
+ *
+ */
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <typeinfo>
+#include <set>
+using std::cout;
+using std::endl;
+using std::string;
+using std::set;
+using std::type_info;
+
+class Observer
+{
+public:
+  virtual ~Observer() {}
+  virtual void update(const string& event) = 0;
+};
+
+class Subject
+{
+protected:
+  set<Observer*> _observers;
+public:
+  virtual ~Subject() {}
+  virtual void addOberver(Observer* observer) = 0;
+  virtual void notifyObserver() = 0;
+};
+
+class ConcreteObserver : public Observer
+{
+protected:
+  void update(const string& event)
+  {
+    const type_info& id = typeid(*this);
+    cout << id.name() << " updated from " << event << endl;
+  }
+public:
+  virtual ~ConcreteObserver() {}
+};
+
+class ConcreteSubject : public Subject
+{
+public:
+  virtual ~ConcreteSubject() {}
+  virtual void addOberver(Observer* observer)
+  {
+    _observers.insert(observer);
+  }
+  virtual void notifyObserver()
+  {
+    for(set<Observer*>::iterator it = _observers.begin(); it != _observers.end(); it++)
+    {
+      const type_info& id = typeid(*this);
+      string event = id.name();
+      (*it)->update(event);
+    }
+  }
+};
+
+
+int main()
+{
+  ConcreteSubject subject;
+  ConcreteObserver observer_a, observer_b, observer_c;
+  subject.addOberver(&observer_a);
+  subject.addOberver(&observer_b);
+  subject.addOberver(&observer_c);
+  subject.notifyObserver();
+  return EXIT_SUCCESS;
+}
