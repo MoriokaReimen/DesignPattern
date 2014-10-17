8cfed886ef35f341e99e4d9be0cb8a91f37f6f71
Kei <Kei@kuru-kuru.(none)>
2014-10-17 10:33:56 +0900

    Add Visitor.cpp

---
+++ C++/Visitor/Visitor.cpp
@@ -0,0 +1,68 @@
+/*
+ * Prototype Pattern
+ * Author: Kei Nakata
+ * Data: Oct.6.2014
+ * Represent an operation to be performed on the elements
+ * of an objectstructure. Visitor lets you define a new operation without
+ * changing the class of the elements on which it operates.
+ */
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using std::cout;
+using std::endl;
+using std::string;
+class Element;
+class ConcreteElement;
+class Visitor;
+class ConcreteVisitor;
+
+class Element
+{
+public:
+  virtual ~Element() {}
+  virtual void accept(Visitor* visitor) = 0;
+  virtual string operation() = 0;
+};
+
+class Visitor
+{
+public:
+  virtual ~Visitor() {}
+  virtual void visitElement(Element* element) = 0;
+};
+
+class ConcreteVisitor : public Visitor
+{
+public:
+  virtual ~ConcreteVisitor() {}
+  void visitElement(Element* element)
+  {
+    cout << element->operation() << endl;
+  }
+};
+
+class ConcreteElement : public Element
+{
+public:
+  ~ConcreteElement() {}
+  void accept(Visitor* visitor)
+  {
+    visitor -> visitElement(this);
+  }
+
+  string operation()
+  {
+    return "Hello I am Element";
+  }
+};
+
+
+int main()
+{
+  ConcreteVisitor visitor;
+  ConcreteElement element;
+  element.accept(&visitor);
+
+  return EXIT_SUCCESS;
+}
