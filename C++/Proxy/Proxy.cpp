fd5a5a9b4c3bb211ffa192b3a86055d1597f7bfa
kei <menschenajger.mark.neun@gmail.com>
2014-10-20 10:29:17 +0900

    Add Comments to Proxy.cpp

--- C++/Proxy/Proxy.cpp
+++ C++/Proxy/Proxy.cpp
@@ -2,7 +2,8 @@
  * Proxy Pattern
  * Author: Kei Nakata
  * Date: Oct.6.2014
- * Provide a surrogate or placeholder for another object to control access to it.
+ * Provide a surrogate or placeholder for another object to control
+ * access to it.
  */
 #include <iostream>
 #include <cstdlib>
@@ -12,47 +13,47 @@ using std::endl;
 class Subject
 {
 public:
-    Subject() {};
-    virtual ~Subject() {}
-    virtual void Request() = 0;
+  Subject() {};
+  virtual ~Subject() {}
+  virtual void Request() = 0;
 };
 
 class ConcreteSubject : public Subject
 {
 protected:
-    friend class Proxy;
-    ConcreteSubject() {}
+  friend class Proxy;
+  ConcreteSubject() {}
 public:
-    virtual ~ConcreteSubject() {}
-    virtual void Request()
-    {
-        cout << "Hello World!!" << endl;
-    }
+  virtual ~ConcreteSubject() {}
+  virtual void Request()
+  {
+    cout << "Hello World!!" << endl;
+  }
 };
 
 class Proxy : Subject
 {
 private:
-    ConcreteSubject* a;
+  ConcreteSubject* a;
 public:
-    Proxy() : a() {}
-    virtual ~Proxy()
-    {
-        if(a)
-            delete a;
-    }
+  Proxy() : a() {}
+  virtual ~Proxy()
+  {
+    if(a)
+      delete a;
+  }
 
-    void Request()
-    {
-        if(!a) a = new ConcreteSubject;
-        a->Request();
-    }
+  void Request()
+  {
+    if(!a) a = new ConcreteSubject;
+    a->Request();
+  }
 };
 
 int main()
 {
-    Proxy a;
-    a.Request();
+  Proxy a;
+  a.Request();
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
