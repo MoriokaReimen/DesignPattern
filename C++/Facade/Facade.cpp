eeb2523f09014589cde7f0e8edc459415953bdbf
kei <menschenajger.mark.neun@gmail.com>
2014-10-20 10:19:54 +0900

    Add Comments to Facade.cpp

--- C++/Facade/Facade.cpp
+++ C++/Facade/Facade.cpp
@@ -1,8 +1,10 @@
 /*
  * Facade Pattern
  * Author: Kei Nakata
- * Data: Oct.6.2014
- *
+ * Date: Oct.6.2014
+ * Provide a unified interface to a set of interfaces in a subsystem.
+ * Facade defines aa higher-level interface that makes the subsystem
+ * easier to  use.
  */
 #include <iostream>
 #include <cstring>
