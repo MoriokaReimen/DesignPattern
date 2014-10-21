d95d707db5c30889f4f128d7831e72b0deb33ef9
kei <menschenajger.mark.neun@gmail.com>
2014-10-21 10:05:42 +0900

    add comments to Strategy.cpp

--- C++/Strategy/Strategy.cpp
+++ C++/Strategy/Strategy.cpp
@@ -1,8 +1,10 @@
 /*
  * Strategy pattern
  * Author: reimen
- * Data: Oct.13.2014
- *
+ * Date: Oct.13.2014
+ * Define a family of algorithms, encapsulate each one, make them
+ * interchangeable Strategy lets the algorithm vary independently from
+ * clients that use it.
  */
 #include <iostream>
 #include <cstdlib>
@@ -12,39 +14,38 @@ using std::endl;
 class Strategy
 {
 public:
-    virtual ~Strategy() {}
-    virtual void algorithm() = 0;
+  virtual ~Strategy() {}
+  virtual void algorithm() = 0;
 };
 
 class ConcreteStrategy : public Strategy
 {
 public:
-    virtual ~ConcreteStrategy() {}
-    void algorithm()
-    {
-        cout << "Hello" << endl;
-    }
+  virtual ~ConcreteStrategy() {}
+  void algorithm()
+  {
+    cout << "Hello" << endl;
+  }
 };
-
 class Context
 {
 protected:
-    Strategy* _strategy;
+  Strategy* _strategy;
 public:
-    Context(): _strategy(new ConcreteStrategy) {}
-    virtual ~Context()
-    {
-        delete _strategy;
-    }
-    void operate()
-    {
-        _strategy->algorithm();
-    }
+  Context(): _strategy(new ConcreteStrategy) {}
+  virtual ~Context()
+  {
+    delete _strategy;
+  }
+  void operate()
+  {
+    _strategy->algorithm();
+  }
 
 };
 int main()
 {
-    Context a;
-    a.operate();
-    return EXIT_SUCCESS;
+  Context a;
+  a.operate();
+  return EXIT_SUCCESS;
 }
