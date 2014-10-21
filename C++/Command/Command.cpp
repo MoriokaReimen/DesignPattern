04eb5751783eabc5af0fb6dd6fdb448385e14814
kei <menschenajger.mark.neun@gmail.com>
2014-10-21 09:13:25 +0900

    modify Command.cpp

--- C++/Command/Command.cpp
+++ C++/Command/Command.cpp
@@ -1,60 +1,60 @@
 /*
- * Prototype Pattern
+ * Command Pattern
  * Author: Kei Nakata
  * Data: Oct.6.2014
- *
+ * Encapsulate a request as an object, thereby letting you parameterize
+ * clients with different requests, queue or log requests, and support
+ * undoable operations.
  */
 #include <iostream>
-#include <cstring>
 #include <cstdlib>
 #include <list>
 using std::cout;
 using std::endl;
-using std::string;
 using std::list;
 
 class Command
 {
 public:
-    Command() {}
-    virtual ~Command(){}
-    virtual void execute() = 0;
+  Command() {}
+  virtual ~Command() {}
+  virtual void execute() = 0;
 };
 
 class ConcreteCommand : public Command
 {
 public:
-    void execute()
-    {
-        cout << "Hello" << endl;
-    }
+  void execute()
+  {
+    cout << "Hello" << endl;
+  }
 };
 
 class Invoker
 {
-    list<Command*> _command_list;
+  list<Command*> _command_list;
 public:
-    void execute()
-    {
-        while(!_command_list.empty())
-        {
-            _command_list.front()->execute();
-            _command_list.pop_front();
-        }
-    }
-    void store(Command* cmd)
-    {
-        _command_list.push_back(cmd);
-    }
+  void execute()
+  {
+    while(!_command_list.empty())
+      {
+        _command_list.front()->execute();
+        _command_list.pop_front();
+      }
+  }
+  void store(Command* cmd)
+  {
+    _command_list.push_back(cmd);
+  }
 };
 
 int main()
 {
-    Invoker a;
-    ConcreteCommand b, c, d;
-    a.store(&b);
-    a.store(&c);
-    a.store(&d);
-    a.execute();
-    return EXIT_SUCCESS;
+  Invoker a;
+  ConcreteCommand b, c, d;
+  a.store(&b);
+  a.store(&c);
+  a.store(&d);
+  a.execute();
+  return EXIT_SUCCESS;
 }
