ccd18eb881b208fcd199d569cc7eab845793f8be
Kei Nakata <menschenjager.mark.neun@gmail.com>
2014-11-01 23:14:25 +0900

    Add Barrier.cpp

---
+++ Threading/Barrier/Barrier.cpp
@@ -0,0 +1,14 @@
+#include <atomic>
+#include <cstdlib>
+#include <thread>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello" << endl;
+    return EXIT_SUCCESS;
+}
+
+
